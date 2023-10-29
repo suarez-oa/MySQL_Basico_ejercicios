4ccdb991ffb70e4a4b2e9fb66d103295b756bc84
suarez-oa <121586904+suarez-oa@users.noreply.github.com>
2023-10-29 16:33:24 -0300

    Add files via upload

--- Oa Suarez MySQL Ejercicios Basicos.sql
+++ Oa Suarez MySQL Ejercicios Basicos.sql
@@ -241,3 +241,80 @@ SELECT nombre, precio
 FROM producto
 WHERE precio >= 180 
 ORDER BY precio DESC, nombre ASC; 
+
+
+
+-- 1.1.4 Consultas multitabla (Composición interna)
+-- Resuelva todas las consultas utilizando la sintaxis de SQL1 y SQL2.
+
+-- 1 Devuelve una lista con el nombre del producto, precio y nombre de fabricante de 
+-- todos los productos de la base de datos.
+SELECT p.nombre, p.precio, f.nombre
+FROM producto p, fabricante f
+WHERE f.id = p.id_fabricante;
+
+-- 2 Devuelve una lista con el nombre del producto, precio y nombre de fabricante de todos 
+-- los productos de la base de datos. Ordene el resultado por el nombre del fabricante, 
+-- por orden alfabético.
+SELECT p.nombre, p.precio, f.nombre
+FROM producto p, fabricante f 
+WHERE f.id = p.id_fabricante
+ORDER BY f.nombre ASC;
+
+-- 3 Devuelve una lista con el identificador del producto, nombre del producto, 
+-- identificador del fabricante y nombre del fabricante, de todos 
+-- los productos de la base de datos.
+SELECT p.id, p.nombre, p.id_fabricante, f.nombre
+FROM producto p, fabricante f
+WHERE f.id = p.id_fabricante;
+
+-- 4 Devuelve el nombre del producto, su precio y el nombre de su fabricante, 
+-- del producto más barato.
+SELECT p.nombre, p.precio, f.nombre
+FROM producto p, fabricante f
+WHERE f.id = p.id_fabricante 
+ORDER BY p.precio ASC 
+LIMIT 1;
+
+-- 5 Devuelve el nombre del producto, su precio y el nombre de su fabricante, 
+-- del producto más caro.
+SELECT p.nombre, p.precio, f.nombre
+FROM producto p, fabricante f
+WHERE f.id = p.id_fabricante 
+ORDER BY p.precio DESC 
+LIMIT 1;
+
+-- 6 Devuelve una lista de todos los productos del fabricante Lenovo.
+SELECT p.*, f.nombre
+FROM producto p, fabricante f
+WHERE f.id = p.id_fabricante AND  f.nombre LIKE '%lenovo%';
+
+-- 7 Devuelve una lista de todos los productos del fabricante Crucial 
+-- que tengan un precio mayor que 200.
+SELECT p.*, f.nombre
+FROM producto p, fabricante f
+WHERE f.id = p.id_fabricante AND  f.nombre LIKE '%Crucial%' AND p.precio >200;
+
+-- 8 Devuelve un listado con el nombre y el precio de todos los productos de los 
+-- fabricantes cuyo nombre termine por la vocal e.
+SELECT p.nombre, p.precio, f.nombre
+FROM producto p
+INNER JOIN fabricante f ON p.id_fabricante = f.id
+WHERE f.nombre LIKE '%e';
+
+-- 9 Devuelve un listado con el nombre y el precio de todos los productos cuyo 
+-- nombre de fabricante contenga el carácter w en su nombre.
+SELECT p.nombre, p.precio, f.nombre
+FROM producto p
+INNER JOIN fabricante f ON p.id_fabricante = f.id
+WHERE f.nombre LIKE '%w%';
+
+-- 10 Devuelve un listado con el nombre de producto, precio y nombre de fabricante, 
+-- de todos los productos que tengan un precio mayor o igual a 180. 
+-- Ordene el resultado en primer lugar por el precio (en orden descendente) 
+-- y en segundo lugar por el nombre del fabricante (en orden ascendente)
+SELECT p.nombre, p.precio, f.nombre
+FROM producto p 
+INNER JOIN fabricante f ON p.id_fabricante = f.id
+WHERE p.precio >= 180
+ORDER BY  p.precio DESC, f.nombre  ASC;
